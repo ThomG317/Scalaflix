5840a4f4086bf30e7932ce9312bde28f9e54591b
Thomas Gaubert <thomas.gaubert49@gmail.com>
2021-04-22 17:20:30 +0200

    question5-fix: Actor constructor fixed

--- src/main/scala/model/Actor.scala
+++ src/main/scala/model/Actor.scala
@@ -1,9 +1,7 @@
 package model
 
 class Actor (val id: Int, val name: String, val surname: String) {
-
   def this(name: String, surname: String) {
-    val id: Int = Math.random().toInt
-    this(id, name, surname)
+    this(Math.random().toInt, name, surname)
   }
 }
